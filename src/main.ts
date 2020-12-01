679026a220ad02a7d32c0b7f0b811da723d586d0
zhangli <zhangli40@rfchina.com>
2020-12-01 15:16:48 +0800

    feat:提交代码

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+/*
+ * @Date: 2020-12-01 15:01:33
+ * @LastEditors: zhangyuge
+ * @LastEditTime: 2020-12-01 15:14:45
+ * @FilePath: \partd:\rf-dev\vue3-admin-demo\src\main.ts
+ */
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
 
-createApp(App)
-  .use(store)
-  .use(router)
-  .mount("#app");
+import ElementPlus from 'element-plus';
+import 'element-plus/lib/theme-chalk/index.css';
+
+const app = createApp(App);
+app.use(ElementPlus);
+app.use(store);
+app.use(router);
+app.mount('#app');
