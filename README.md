cbd9c64a7199ede3db63b22c191965394ccb6e0e
zhangli <zhangli40@rfchina.com>
2020-12-01 16:52:25 +0800

    feat:新增简单的demo一看就懂了

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
+<!--
+ * @Date: 2020-12-01 15:02:12
+ * @LastEditors: zhangyuge
+ * @LastEditTime: 2020-12-01 16:37:55
+ * @FilePath: \partd:\rf-dev\vue3-admin-demo\README.md
+-->
 # vue3-admin-demo
 
 ## Project setup
@@ -27,3 +33,5 @@ yarn lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+[https://vue3js.cn/docs/zh/guide/introduction.html](https://vue3js.cn/docs/zh/guide/introduction.html)
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,34 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-  </div>
+    <div class="home">
+        <img alt="Vue logo" src="../assets/logo.png" />
+        <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+        {{ state.a }}
+        <button @click="increment">加一</button>
+    </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
-
-export default defineComponent({
-  name: "Home",
-  components: {
-    HelloWorld
-  }
-});
+// import { defineComponent } from 'vue';
+import { reactive, onMounted } from 'vue';
+import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+export default {
+    setup() {
+        const state = reactive({
+            a: 0
+        });
+        function increment() {
+            state.a++;
+        }
+        onMounted(() => {
+            console.log('mounted!');
+        });
+        return {
+            state,
+            increment
+        };
+    },
+    components: {
+        HelloWorld
+    }
+};
 </script>
