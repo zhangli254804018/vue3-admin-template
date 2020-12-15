9a73a471f919cc8e613c3092b134f6e24c103889
254804018@qq.com <254804018@qq.com>
2020-12-15 23:57:19 +0800

    feat:2020-12-15-23:57:19

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,11 @@
+<!--
+ * @Author: your name
+ * @Date: 2020-12-03 22:23:23
+ * @LastEditTime: 2020-12-15 23:57:02
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: /out/vue3-admin-template/src/views/Home.vue
+-->
 <template>
     <div class="home">
         <img alt="Vue logo" src="../assets/logo.png" />
@@ -8,10 +16,10 @@
 </template>
 
 <script lang="ts">
-// import { defineComponent } from 'vue';
+import { defineComponent } from 'vue';
 import { reactive, onMounted } from 'vue';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
-export default {
+export default defineComponent({
     setup() {
         const state = reactive({
             a: 0
@@ -30,5 +38,5 @@ export default {
     components: {
         HelloWorld
     }
-};
+})
 </script>
